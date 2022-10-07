255164ce03fc21f5746b889735ae3234121b81cc
Tomasz <tomaszwoloszyn983@gmail.com>
2022-10-07 09:00:34 +0000

    Create base.html file. Implement Materialize

--- list/views.py
+++ list/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def say_hello(request):
     print("Hello world")
-    return render(request, "../templates/hello.html")
+    return render(request, "list/static/templates/base.html")
