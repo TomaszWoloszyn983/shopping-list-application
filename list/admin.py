2a9b2a4aa122bfc44a8337a81a951cfd0658ff26
Tomasz <tomaszwoloszyn983@gmail.com>
2022-10-07 07:26:43 +0000

    Install Summernote. Update requirements.txt file

--- list/admin.py
+++ list/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+# @admin.site.register(Item)
+# class ItemAdmin(Item):
+#     prepopulated_fields = {'slug': ('name',)}
