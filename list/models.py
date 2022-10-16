f5ac08e7ea2b8e327e957f5f9882a58631eecee1
Tomasz <tomaszwoloszyn983@gmail.com>
2022-10-16 15:19:53 +0000

    Fix item_not_found_error. Display lists in the lists.html template

--- list/models.py
+++ list/models.py
@@ -4,11 +4,11 @@ from django.utils import timezone
 
 
 class List(models.Model):
-    id = models.IntegerField(primary_key=True)
+    # id = models.IntegerField(primary_key=True)
     create_date = models.DateField(default=timezone.now)
     date = str(timezone.now)
     name = models.CharField(blank=False, max_length=50, unique=True, default=("New List"))
-    slug = models.SlugField(blank=False, max_length=50, unique=True, default=("New List"))
+    slug = models.SlugField(blank=False, max_length=50, unique=True, default=(""))
     # items = models.ForeignKey(Item, on_delete=models.CASCADE)
 
     # class Meta:
@@ -22,12 +22,12 @@ class Item(models.Model):
     # quantity variable type is set to char, because 
     # the quantity douesn't have to be one or two units,
     # it also can be 1 kg or 2 liters.
-    id = models.IntegerField(primary_key=True)
+    # id = models.IntegerField(primary_key=True)
     name = models.CharField(blank=False, max_length=50, unique=True, default="")
     slug = models.SlugField(blank=False, max_length=50, unique=True, default="")
     quantity = models.CharField(max_length=10, default='1')
     bought = models.BooleanField(default=False)
-    list_id = models.ForeignKey(List, on_delete=models.CASCADE, related_name="items")
+    # list_id = models.ForeignKey(List, on_delete=models.CASCADE, related_name="items")
 
     class Meta:
         ordering = ['-bought']
